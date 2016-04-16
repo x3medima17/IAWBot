46a5ad1c71ce53da68eb4e237c16a37343b2fe61
Dumitru <dimasavva17@gmail.com>
2016-04-17 02:12:26 +0300

    Tuple implementation

---
+++ src/Application.java
@@ -0,0 +1,5 @@
+/**
+ * Created by dumitru on 17.04.16.
+ */
+public class Application {
+}
---
+++ src/Tuple.java
@@ -0,0 +1,11 @@
+/**
+ * Created by dumitru on 17.04.16.
+ */
+public class Tuple<X, Y> {
+    public final X x;
+    public final Y y;
+    public Tuple(X x, Y y) {
+        this.x = x;
+        this.y = y;
+    }
+}
