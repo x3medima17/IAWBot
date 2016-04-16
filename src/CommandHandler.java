c51b3905b129192c95ea65edbd761269e6b638b4
Dumitru <dimasavva17@gmail.com>
2016-04-17 02:39:33 +0300

    Abstract CommandHandler Class

---
+++ src/CommandHandler.java
@@ -0,0 +1,16 @@
+/**
+ * Created by dumitru on 17.04.16.
+ */
+public  abstract class CommandHandler {
+    public void handle() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void before() {
+
+    }
+
+    public void after(){
+
+    }
+}
