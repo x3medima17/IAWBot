ea6a22acc536bd997812b8edad3b8e99e657b0db
Sergey Pinkevich <sergeipinkevich163@gmail.com>
2016-04-17 16:18:02 +0300

    json parse

--- src/MessageHelper.java
+++ src/MessageHelper.java
@@ -1,6 +1,10 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import java.net.*;
 import java.io.*;
-import java.util.Scanner;
+import java.util.Iterator;
 
 /**
  * Created by serge on 17.04.2016.
@@ -11,36 +15,26 @@ public class MessageHelper {
         //JsonElement resultJson = new JsonParser().parse(command);
         String tempUrl = URLEncoder.encode(command, "utf-8");
         String duckURL = new String("http://api.duckduckgo.com/?q=" + tempUrl + "&format=json&pretty=1");
-        Request request = new Request(duckURL);
-        String answer = request.response;
-
+        Request req = new Request(duckURL);
+        req.setMethod();
+        String answer = req.getResponse();
         return answer;
     }
 
-//    public static String sendMessageToServer(String command) throws IOException {
-//        String tempUrl = URLEncoder.encode(command, "utf-8");
-//        String duckURL = new String("http://api.duckduckgo.com/?q=" + tempUrl + "&format=json&pretty=1");
-//        URL url = new URL(duckURL);
-//        URLConnection connection = url.openConnection();
-//        InputStream response = connection.getInputStream();
-//        String result = convertStreamToString(response);
-//        return result;
-//    }
-//
-//    private static String convertStreamToString(InputStream is) throws IOException {
-//        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-//        StringBuilder sb = new StringBuilder();
-//
-//        String line = null;
-//        try {
-//            while ((line = reader.readLine()) != null) {
-//                sb.append(line + "\n");
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } finally {
-//           is.close();
-//        }
-//        return sb.toString();
-//    }
+    private static String jsonToAnswer(String json) {
+        JSONParser parser = new JSONParser();
+        StringBuilder sb = new StringBuilder();
+        try {
+            Object obj = parser.parse(json);
+            JSONObject jsonObject = (JSONObject) obj;
+            JSONArray textList = (JSONArray) jsonObject.get("RelatedTopics");
+            Iterator<String> iterator = textList.iterator();
+            while (iterator.hasNext()) {
+                sb.append(iterator.next());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
 }
