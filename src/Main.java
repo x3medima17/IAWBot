6c33549b3a3ea70381f6a1dbbb4d7594a82a1836
Yuliya <elektra-88@mail.ru>
2016-04-17 15:32:43 +0300

    Realisation of sending messages

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 
@@ -13,11 +14,11 @@ public class Main {
 
         ArrayList<Tuple> handlers = new ArrayList<>();
         handlers.add(new Tuple("/hello",new HelloHandler()));
+        handlers.add(new Tuple("any natural language",new NaturalLanguageHandler()));
 
 
         Application application = new Application(handlers, bot);
         application.startIoLoop();
-
     }
 
 
@@ -31,6 +32,24 @@ class HelloHandler extends CommandHandler {
     public void handle() {
         System.out.println("This is Hello world!");
 
+
+    }
+
+    public void after() {
+        System.out.println("After method");
+    }
+
+}
+class NaturalLanguageHandler extends CommandHandler {
+
+    public void before() {
+        System.out.println("this is before implementation natural language");
+
+    }
+
+    public void handle() throws UnsupportedEncodingException {
+        System.out.println("This is natural language!");
+        bot.sendMessage(getChatId(), getCommand());
     }
 
     public void after() {
